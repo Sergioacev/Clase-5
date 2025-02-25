4febf661341fb38f931641fa708385b1cc24723d
Sergioacev <acevedoscolrosarioa@gmail.com>
2025-02-24 21:10:50 -0500

    Add files via upload

---
+++ clase5.py
@@ -0,0 +1,14 @@
+import requests, re, json
+
+
+def extractFormRegularExpresion(regex, data):
+    if data: 
+        return re.findall(regex, data)
+    
+data=requests.get("https://raw.githubusercontent.com/elastic/examples/refs/heads/master/Common%20Data%20Formats/apache_logs/apache_logs").text
+regex = r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s-\s-\s\[(\d{2}\/\b[a-zA-Z]{3}\b\/\d{4})\:(\d{2}\:\d{2}\:\d{2})"
+
+resultado= extractFormRegularExpresion(regex, data)
+
+for i in range(len(resultado)):
+    print(f"La IP: {resultado[i][0]} y la fecha es: {resultado[i][1]} y la hora{resultado[i][2]}")
